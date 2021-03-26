da7a7bd190feee8d53ff773e84e1dd5e78ba0cd8
smartbios <red.genomica.smaertbios@gmail.com>
2021-03-26 00:10:37 -0300

    BBDuk

--- src/controllers/biotools.js
+++ src/controllers/biotools.js
@@ -1,10 +1,11 @@
 import { spawn } from 'child_process'
 import os from 'os'
 import path from 'path'
+import Storage from '../models/storage'
 
 
 const databasesRoot = path.join(os.homedir(), 'Databases');
-
+const bbduk  = '/opt/biotools/bbmap/bbduk.sh'
 
 export default {
 
@@ -44,7 +45,7 @@ export default {
     
                 res.json({
                     status: 'success',
-                    msg: 'Blast terminado',
+                    msg: 'Blast finished',
                     result: JSON.parse(result)
                 })
             });
@@ -58,8 +59,80 @@ export default {
 
     },
 
-    bbduk: (req, res ) => {
-        
+    bbduk: async (req, res ) => {
+        try {
+            let fq1 = path.join(os.homedir(), req.body.fq1)
+            let fq2 = path.join(os.homedir(), req.body.fq2)
+            let output = path.join(os.homedir(), `Storage/${req.body.user._id}/results`)
+            let log = ''
+            let params = [  
+                `in1=${fq1}`,
+                `out1=${output}/${req.body.basename}_R1_good.fq.gz`, 
+                `in2=${fq2}`,
+                `out2=${output}/${req.body.basename}_R2_good.fq.gz`, 
+                'ref=/opt/biotools/bbmap/resources/adapters.fa', 
+                `minavgquality=${req.body.minavgquality}`,
+                `trimq=${req.body.trimq}`,
+                `qtrim=${req.body.qtrim}`, 
+                `minlen=${req.body.length}`,
+                'json=t'
+            ]
+            if(!req.body.paired) params.splice(2,2)
+    
+            let  cmd_bbduk = spawn(bbduk, params)
+           
+            cmd_bbduk.stderr.on('data', (data) => {
+                log += data.toString()
+                console.log(data.toString())
+            });
+
+            cmd_bbduk.on('close', (code) => {
+                console.log(`BBDuk process exited with code ${code}`);
+                if(code == 0){
+
+                    let trimfq1 = {
+                        user: req.body.user._id,
+                        filename: `${req.body.basename}_R1_good.fq.gz`,
+                        description: `BBDuk triming result`,
+                        path: `Storage/${req.body.user._id}/results/${req.body.basename}_R1_good.fq.gz`,
+                        type: 'fastq',
+                        category: 'result'
+                    }
+
+                    let trimfq2 = {
+                        user: req.body.user._id,
+                        filename: `${req.body.basename}_R2_good.fq.gz`,
+                        description: `BBDuk triming result`,
+                        path: `Storage/${req.body.user._id}/results/${req.body.basename}_R2_good.fq.gz`,
+                        type: 'fastq',
+                        category: 'result'
+                    }
+
+                    Storage.insertMany([trimfq1, trimfq2])
+                    .then(function(){
+                        console.log("Data inserted")
+                        res.json({
+                            status: 'success',
+                            msg: 'BBDuk finished',
+                            result: JSON.parse(log)
+                        })
+                    }).catch(function(error){
+                        console.log(error)  
+                    });
+                }else{
+                    res.json({
+                        status: 'danger',
+                        msg: 'BBDuk finished with error',
+                        result: ''
+                    })
+                }
+            })
+        } catch (error) {
+            res.status(500).json({
+                status: 'danger',
+                msg: error
+            });
+        }
     }
 
 }
--- src/routes/biotools.js
+++ src/routes/biotools.js
@@ -4,6 +4,7 @@ import Biotools from '../controllers//biotools';
 const route = Router();
 
 route.post( '/blast', Biotools.blast);
+route.post( '/bbduk', Biotools.bbduk);
 
 
 export default route;
